c1f15ae0110111e1b0945b7eea5ab72934a51b22
WangFenjin <wangfenjin@bytedance.com>
2018-12-02 19:54:34 +0800

    impl day00 & day01
    Change-Id: I2e6154783f4ade9ee559e027a2e0c61fbb4ac95b

---
+++ 2018/day01/src/main.rs
@@ -0,0 +1,39 @@
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+use std::path::Path;
+use std::collections::HashMap;
+
+fn main() {
+    let f = File::open("./src/input.txt").unwrap();
+    let mut file = BufReader::new(&f);
+    let mut result = 0;
+    let mut frequency = HashMap::new();
+    let mut inputs = vec![];
+    frequency.insert(0, 1);
+    for line in file.lines() {
+        let l = line.unwrap();
+        let i = l.parse::<i32>().unwrap();
+        result += i;
+        if frequency.contains_key(&result) {
+            println!("{}", result);
+            return;
+        } else {
+            frequency.insert(result, 1);
+            inputs.push(i);
+        }
+    }
+    while true {
+        for i in inputs.iter() {
+            result += i;
+        if frequency.contains_key(&result) {
+            println!("{}", result);
+            return;
+        } else {
+            frequency.insert(result, 1);
+            // inputs.push(i);
+        }
+        }
+    }
+    // println!("{}", result);
+}
